fc3edd7c9bc67af3ae39af3b1aea70dc65e663ec
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-01 11:08:38 -0500

    set up runner for given test cases'

--- runner.rb
+++ runner.rb
@@ -1,4 +1,23 @@
-require_relative 'document_application'
+require_relative 'parser'
 
-application = DocumentApplication.new
-application.run_program
+#####SET UP######
+
+## parse file into objects
+parser = Parser.new
+file = 'input_text.txt'
+created_object = parser.parse_file(file)
+investors = created_object[:investors]
+documents = created_object[:documents]
+
+##find Bob, actual_notes, and super_secret_meeting_notes_jan
+bob = investors.select {|i| i.id == "8675309"}[0]
+actual_notes = documents.select {|i| i.id == "1215"}[0]
+super_secret_meeting_notes_jan = created_object[:documents].select {|i| i.id == "1929"}[0]
+
+##Check access
+bob_results = bob.has_access_to_doc?(actual_notes)
+super_secret_results = super_secret_meeting_notes_jan.all_investors_with_access(investors)
+
+#Print Results 
+puts "Can Bob Jones see the actual_notes.txt? #{bob_results}"
+puts "What are the ids of the Investors can see the super_secret_meeting_notes_jan.pdf? #{super_secret_results.join(", ")}"

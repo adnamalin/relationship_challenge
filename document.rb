22592fa559a9f1ef3df65ce8975094886bf4658c
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-31 21:52:08 -0500

    change ids to strings

--- document.rb
+++ document.rb
@@ -4,8 +4,8 @@ class Document
 
   def initialize(account_details)
     @name = account_details[:name]
-    @size = account_details[:size].to_i
-    @id = account_details[:id].to_i
+    @size = account_details[:size]
+    @id = account_details[:id]
   end
 
 end

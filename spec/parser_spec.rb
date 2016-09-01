f73a3aac6ba29940fbc1f3a36776f0aecb536fc8
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-31 21:17:34 -0500

    testing parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,17 +1,31 @@
 require_relative '../parser'
 require_relative '../investor'
+require_relative '../account'
+require_relative '../document'
 
-RSpec.describe Investor do
+RSpec.describe Parser do
 
   let(:parser) { Parser.new }
 
   describe 'create investors' do
-
     it 'creates investors from array' do
       investors = [":Investors:", "First Name, Last Name, ID", "Amanda, Lin, 1234"]
       expect(parser.create_investors(investors)[0]).to be_a Investor
     end
+  end
 
+  describe 'create accounts' do
+    it 'creates accounts from array' do
+      accounts = [":Accounts:", "Account Name, ID", "Global Opportunity, 2525"]
+      expect(parser.create_accounts(accounts)[0]).to be_a Account
+    end
+  end
+
+  describe 'create documents' do
+    it 'creates documents from array' do
+      documents = [":Accounts:", "Account Name, ID", "Global Opportunity, 2525"]
+      expect(parser.create_documents(documents)[0]).to be_a Document
+    end
   end
 
 end

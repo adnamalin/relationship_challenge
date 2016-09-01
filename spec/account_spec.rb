d735e0a59b83f1b53cfe754d323111871d2aaec6
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-31 21:18:06 -0500

    pass all acct and doc test

---
+++ spec/account_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../account'
+
+RSpec.describe Account do
+
+  let(:account) { Account.new(name: "Backstop Solutions" ,id: "1234") }
+
+  describe 'attributes' do
+    it 'has a name' do
+      expect(account.name).to eq 'Backstop Solutions'
+    end
+
+    it 'has an id' do
+      expect(account.id).to eq 1234
+    end
+  end
+end
---
+++ spec/document_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../document'
+
+RSpec.describe Document do
+
+  let(:document) { Document.new(name: "Backstop Solutions" ,size: "4815162342", id: "1234") }
+
+  describe 'attributes' do
+    it 'has a name' do
+      expect(document.name).to eq 'Backstop Solutions'
+    end
+
+    it 'has a size' do
+      expect(document.size).to eq 4815162342
+    end
+
+    it 'has an id' do
+      expect(document.id).to eq 1234
+    end
+  end
+end

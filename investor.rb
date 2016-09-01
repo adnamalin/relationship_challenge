e48618a6031ac93984ff8798f07386f052b15d24
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-31 22:28:14 -0500

    add accounts attr

--- investor.rb
+++ investor.rb
@@ -1,12 +1,13 @@
 class Investor
 
-  attr_reader :first_name, :last_name, :id, :documents
+  attr_reader :first_name, :last_name, :id, :documents, :accounts
 
   def initialize(investor_details)
     @first_name = investor_details[:first_name]
     @last_name = investor_details[:last_name]
     @id = investor_details[:id]
     @documents = []
+    @accounts = []
   end
 
   def direct_access_to_doc?(document_id)
